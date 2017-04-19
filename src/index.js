4297e98706f4fe4f7abbf8720a56c67ffcba0812
Derek Davis <derek.davis@Dereks-MacBook-Pro.local>
2017-04-18 21:46:17 -0500

    reordering original folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import App from './original/App';
+import App from './0. original/App';
 //import App from './1. react/7. forms/App';
-import App from './3. react-redux/App';
+//import App from './3. react-redux/App';
 
 ReactDOM.render(
   <App />,
